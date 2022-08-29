f6598f7ba5c1d834ac4eff22fea255ba3b843f20
christy313 <christycreativity@gmail.com>
2022-08-29 17:47:26 -0400

    Use lato font in index.css

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = () => {
   return (
     <div className="tc">
-      <h2 className="f3">{"Hi, your current rank is..."}</h2>
+      <h2 className="f1">{"Hi, your current rank is..."}</h2>
       <p className="f1">{"#5"}</p>
     </div>
   );
