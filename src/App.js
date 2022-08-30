5b88cdbf54a99c8bfd66dcc0019ea346d406524a
christy313 <christycreativity@gmail.com>
2022-08-29 21:20:43 -0400

    Add FaceRecognition component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import Clarifai from "clarifai";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+
+const app = new Clarifai.App({
+  apiKey: "3261c928d20848b6a4b8674d822130a8",
+});
 
 export default class App extends Component {
   constructor() {
@@ -18,10 +24,17 @@ export default class App extends Component {
   };
 
   onButtonSubmit = () => {
-    app.models.predict("", "url").then(
-      function (response) {},
-      function (err) {}
-    );
+    app.models
+      .predict(
+        "53e1df302c079b3db8a0a36033ed2d15",
+        "https://imgs.search.brave.com/qXpEkNeP0l2xUrG9o30HeeWsdYPhZzf3TyNPwvRLh8Y/rs:fit:1200:750:1/g:ce/aHR0cHM6Ly8xNzM0/ODExMDUxLnJzYy5j/ZG43Ny5vcmcvZGF0/YS9pbWFnZXMvZnVs/bC8zNzE5MjUvZ29v/Z2xlLW1lZXQtaG93/LXRvLWNoYW5nZS1s/YXlvdXQtd2l0aC00/OS1wZXJzb24tZ3Jp/ZC12aWV3LmpwZw"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
   };
 
   render() {
@@ -34,7 +47,7 @@ export default class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecogniton /> */}
+        <FaceRecognition />
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FaceRecognition = () => {
+  return (
+    <div className="center">
+      <img
+        src={
+          "https://imgs.search.brave.com/qXpEkNeP0l2xUrG9o30HeeWsdYPhZzf3TyNPwvRLh8Y/rs:fit:1200:750:1/g:ce/aHR0cHM6Ly8xNzM0/ODExMDUxLnJzYy5j/ZG43Ny5vcmcvZGF0/YS9pbWFnZXMvZnVs/bC8zNzE5MjUvZ29v/Z2xlLW1lZXQtaG93/LXRvLWNoYW5nZS1s/YXlvdXQtd2l0aC00/OS1wZXJzb24tZ3Jp/ZC12aWV3LmpwZw"
+        }
+        alt="Image detection"
+      />
+    </div>
+  );
+};
+
+export default FaceRecognition;
