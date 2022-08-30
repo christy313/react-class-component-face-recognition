e861059d6571e42a8f57e3ddc51316d89baff8c2
christy313 <christycreativity@gmail.com>
2022-08-29 20:49:01 -0400

    Add onInputChange & onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useCallback } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -6,13 +6,31 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("hi");
+  };
+
   render() {
     return (
       <div>
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecogniton /> */}
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className="tc">
       <p className="f3">{"Detect faces in your pictures"}</p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center b--blue" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-blue b--light-blue">
+          <input
+            onChange={onInputChange}
+            className="f4 pa2 w-70 center b--blue"
+            type="text"
+          />
+          <button
+            onClick={onButtonSubmit}
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-blue b--light-blue"
+          >
             Detect
           </button>
         </div>
