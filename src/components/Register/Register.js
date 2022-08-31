ec4e4b3d8910dd9cc12095a1e89b7b845bf042c3
christy313 <christycreativity@gmail.com>
2022-08-30 20:47:06 -0400

    Fix error Form submission canceled because the form is not connected, replace form with div in both SignIn.js & Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,11 +4,11 @@ const Register = ({ onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure tc">
+        <div className="measure tc">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Register</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f3" for="name">
+              <label className="db fw6 lh-copy f3" htmlFor="name">
                 Name
               </label>
               <input
@@ -19,7 +19,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className="mt3">
-              <label className="db fw6 lh-copy f3" for="email">
+              <label className="db fw6 lh-copy f3" htmlFor="email">
                 Email
               </label>
               <input
@@ -30,7 +30,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f3" for="password">
+              <label className="db fw6 lh-copy f3" htmlFor="password">
                 Password
               </label>
               <input
@@ -49,7 +49,7 @@ const Register = ({ onRouteChange }) => {
               value="Register"
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,11 +4,11 @@ const SignIn = ({ onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure tc">
+        <div className="measure tc">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f3" for="email-address">
+              <label className="db fw6 lh-copy f3" htmlFor="email-address">
                 Email
               </label>
               <input
@@ -19,7 +19,7 @@ const SignIn = ({ onRouteChange }) => {
               />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f3" for="password">
+              <label className="db fw6 lh-copy f3" htmlFor="password">
                 Password
               </label>
               <input
@@ -47,7 +47,7 @@ const SignIn = ({ onRouteChange }) => {
               Register
             </p>
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
